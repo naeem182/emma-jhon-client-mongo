f0e1973702f04c6394a5efa142cadefd9abaaa2f
naeem <naeemislamjr182@gmail.com>
2023-11-01 01:21:11 +0600

    handle dynamic page

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,7 +9,8 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
     const { count } = useLoaderData();
-    console.log(count)
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    // console.log(count)
 
 
     /**
@@ -18,7 +19,7 @@ const Shop = () => {
      * TODO 3: get the current page
     */
 
-    const itemsPerPage = 10;
+    // const itemsPerPage = 10;
 
     const numberOfPages = Math.ceil(count / itemsPerPage);
 
@@ -84,6 +85,12 @@ const Shop = () => {
         setCart([]);
         deleteShoppingCart();
     }
+    const handleItemsPerPage = e => {
+        const val = parseInt(e.target.value);
+        console.log(val);
+        setItemsPerPage(val);
+        setCurrentPage(0);
+    }
 
     return (
         <div className='shop-container'>
@@ -118,6 +125,13 @@ const Shop = () => {
                         key={page}
                     >{page}</button>)
                 }
+
+                <select value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
