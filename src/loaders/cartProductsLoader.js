0ac9a43bbff246572681e27de4efd56e7b04b55a
naeem <naeemislamjr182@gmail.com>
2023-11-01 20:39:05 +0600

    load and map only ordered product

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,16 +1,26 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
-const cartproductLoader = async () => {
-    const loadedproduct = await fetch('http://localhost:5000/product');
-    const product = await loadedproduct.json();
 
+const cartproductLoader = async () => {
     // if cart data is in database, you have to use async await
     const storedCart = getShoppingCart();
+    const storedCartIds = Object.keys(storedCart)
+    console.log(storedCartIds)
+
+    const loadedProducts = await fetch('http://localhost:5000/productByIds', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(storedCartIds)
+    });
+
+    const products = await loadedProducts.json();
 
     const savedCart = [];
 
     for (const id in storedCart) {
-        const addedProduct = product.find(pd => pd._id === id);
+        const addedProduct = products.find(pd => pd._id === id);
         if (addedProduct) {
             const quantity = storedCart[id];
             addedProduct.quantity = quantity;
@@ -19,11 +29,10 @@ const cartproductLoader = async () => {
     }
 
     // if you need to send two things
-    // return [product, savedCart]
+    // return [products, savedCart]
     // another options
-    // return { product, cart: savedCart }
+    // return { products, cart: savedCart }
 
     return savedCart;
 }
-
 export default cartproductLoader;
